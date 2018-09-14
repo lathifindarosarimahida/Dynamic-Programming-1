b270642d9536ef584025864b76ed3d4c2d012e7d
USER <USER@DESKTOP-LSF5PJH>
2018-09-15 04:12:08 +0700

    inputan

--- src/hitungfibonacci/Tugas02.java
+++ src/hitungfibonacci/Tugas02.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitungfibonacci;
-
-/**
- *
- * @author USER
- */
+import java.util.Scanner;
 public class Tugas02 {
+    public static void main(String[] args)
+    {
+        System.out.println("Nama    : Lathifinda Rosari Mahida");
+        System.out.println("Kelas   : X RPL 2 ");
+        System.out.println("Absen   : 19");
+        
+        int [] Berat = new int []{4, 5, 2, 3, 7};
+        int [] Bayaran = new int []{5000, 7000, 2000, 3000, 10000};
+        
+    }
+   
     
 }
