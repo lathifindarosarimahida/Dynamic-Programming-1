8cd934dab37a6e58ae2d919e1f9ff21a1110c3de
USER <USER@DESKTOP-LSF5PJH>
2018-09-15 04:13:26 +0700

    inputan 2

--- src/hitungfibonacci/Tugas02.java
+++ src/hitungfibonacci/Tugas02.java
@@ -10,7 +10,26 @@ public class Tugas02 {
         int [] Berat = new int []{4, 5, 2, 3, 7};
         int [] Bayaran = new int []{5000, 7000, 2000, 3000, 10000};
         
+        int berat = Input();
+        Proses(berat, Berat, Bayaran);
     }
-   
+    private static int Input(){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Berat Barang: ");
+        int berat = scan.nextInt();
+        
+        return berat;
+    }
     
+    private static void Proses(int berat, int[] Berat, int[] Bayaran)
+    {
+        int jumlah = 0;
+        for (int i = 0; i < Berat.length; i++)
+        {
+            
+        }
+    }
 }
+    
+
