1f5e15196eaf1dca6d6b7cb2c84779b768a3fd84
USER <USER@DESKTOP-LSF5PJH>
2018-09-15 04:05:21 +0700

    Identitas

--- src/hitungfibonacci/Tugas01.java
+++ src/hitungfibonacci/Tugas01.java
@@ -1,4 +1,13 @@
 package hitungfibonacci;
+import java.util.Scanner;
 public class Tugas01 {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Nama    : Lathifinda Rosari Mahida");
+        System.out.println("Kelas   : X RPL 2 ");
+        System.out.println("Absen   : 19");
+        
+       
 }
+}
+
